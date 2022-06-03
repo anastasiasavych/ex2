7c5771fe4204adbd2c8726b437f029def475f06e
anastasiasavych <99317597+anastasiasavych@users.noreply.github.com>
2022-06-03 11:41:34 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Two t1 = new Two(3,4,5);
+    }
+}
---
+++ src/com/company/Two.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Two extends One{
+    double c;
+
+    public Two(double a,double b, double c){
+        super(a,b);
+    }
+}
